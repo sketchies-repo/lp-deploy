55f4e4a91018e55266858be02e55dcae184cc686
sketchies-repo <sketchies.io@gmail.com>
2024-02-16 09:32:53 +0100

    ajout CNAME et changement fond noir MainLayout

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Sketchies Landing Page
 
-Discover the project here : [Sketchies](https://sketchies-repo.github.io/lp-deploy)
+Discover the project here : [Sketchies](https://sketchies.art)
--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
-import ComingSoon from './page/ComingSoon'
 import MainLayout from './page/MainLayout'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<ComingSoon />} />
+        <Route exact path="/" element={<MainLayout />} />
       </Routes>
     </BrowserRouter>
   )
